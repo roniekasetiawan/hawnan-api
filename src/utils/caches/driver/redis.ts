8413829acfa861a42b8d26f1964da9d5dfbb197a
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-11-27 23:33:33 +0700

    refactor: Update cache.ts and redis.ts for consistency

--- src/utils/caches/driver/redis.ts
+++ src/utils/caches/driver/redis.ts
@@ -1,12 +1,12 @@
-import { Cache, CacheConfig, CacheKey } from "@/utils/caches/cache"
-import Redis from "ioredis"
+import { Cache, CacheConfig, CacheKey } from '@/utils/caches/cache';
+import Redis from 'ioredis';
 
 export default class RedisImpl implements Cache {
-  private connections: Redis[] = []
+  private connections: Redis[] = [];
 
   constructor({ config }: { config: CacheConfig }) {
     if (config.connections.length == 0) {
-      throw new Error("the connections cannot be less than or equal 0")
+      throw new Error('the connections cannot be less than or equal 0');
     }
 
     for (let c of config.connections) {
@@ -15,46 +15,46 @@ export default class RedisImpl implements Cache {
         port: c.port,
         password: c.password,
         username: c.username,
-      })
-      this.connections.push(redis)
+      });
+      this.connections.push(redis);
     }
   }
 
   async del({ keys }: { keys: CacheKey[] }): Promise<{ err?: Error }> {
     try {
       for (let c of this.connections) {
-        await c.del(...keys)
+        await c.del(...keys);
       }
 
-      return {}
+      return {};
     } catch (err: Error | any) {
-      return { err }
+      return { err };
     }
   }
 
   async set({ key, value }: { key: CacheKey; value: string }): Promise<{ err?: Error }> {
     try {
       for (let c of this.connections) {
-        await c.set(key, value)
+        await c.set(key, value);
       }
 
-      return {}
+      return {};
     } catch (err: Error | any) {
-      return { err }
+      return { err };
     }
   }
 
-  async get({ key }: { key: CacheKey }): Promise<{ data?: string, err?: Error }> {
+  async get({ key }: { key: CacheKey }): Promise<{ data?: string; err?: Error }> {
     try {
       for (let c of this.connections) {
-        const value = await c.get(key)
+        const value = await c.get(key);
         if (value) {
-          return { data: value }
+          return { data: value };
         }
       }
-      return {}
+      return {};
     } catch (err: Error | any) {
-      return { err }
+      return { err };
     }
   }
 }
