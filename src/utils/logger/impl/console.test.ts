9f93a6099ca81df8c7ee62cd54ef9878d701ff95
irda islakhu afa <irdhaislakhuafa@gmail.com>
2025-11-27 23:58:06 +0700

    test: update function calls in console logger tests

--- src/utils/logger/impl/console.test.ts
+++ src/utils/logger/impl/console.test.ts
@@ -1,7 +1,7 @@
-import { LogFormat, type Config } from "../logger"
-import { ConsoleLogger } from "./console"
+import { LogFormat, type Config } from '../logger';
+import { ConsoleLogger } from './console';
 
-describe("console logger", async () => {
+describe('console logger', async () => {
   let log: ConsoleLogger;
 
   beforeEach(() => {
@@ -9,22 +9,21 @@ describe("console logger", async () => {
       format: LogFormat.JSON,
       levels: [],
     } as Config);
-  })
+  });
 
-  test("info: should print log with info level", async () => {
-    log.info({ message: "Hello World", err: new Error() })
-  })
+  test('info: should print log with info level', async () => {
+    log.info({ message: 'Hello World', err: new Error() });
+  });
 
-  test("warn: should print log with warn level", async () => {
-    log.warn({ message: "Hello World", err: new Error() })
-  })
+  test('warn: should print log with warn level', async () => {
+    log.warn({ message: 'Hello World', err: new Error() });
+  });
 
-  test("debug: should print log with debug level", async () => {
-    log.debug({ message: "Hello World", err: new Error() })
-  })
+  test('debug: should print log with debug level', async () => {
+    log.debug({ message: 'Hello World', err: new Error() });
+  });
 
-  test("error: should print log with error level", async () => {
-    log.error({ message: "Hello World", err: new Error() })
-  })
-
-})
+  test('error: should print log with error level', async () => {
+    log.error({ message: 'Hello World', err: new Error() });
+  });
+});
